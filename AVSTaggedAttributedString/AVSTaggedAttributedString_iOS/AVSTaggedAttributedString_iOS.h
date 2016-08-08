275e45028e4c9a92c6aaa71eae6afed08c45b1dc
Andrei Sherstniuk <sherstnyuk.a@gmail.com>
2016-08-08 15:28:24 +0300

    Create Project.

---
+++ AVSTaggedAttributedString/AVSTaggedAttributedString_iOS/AVSTaggedAttributedString_iOS.h
@@ -0,0 +1,19 @@
+//
+//  AVSTaggedAttributedString_iOS.h
+//  AVSTaggedAttributedString_iOS
+//
+//  Created by Andrei Sherstniuk on 8/8/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AVSTaggedAttributedString_iOS.
+FOUNDATION_EXPORT double AVSTaggedAttributedString_iOSVersionNumber;
+
+//! Project version string for AVSTaggedAttributedString_iOS.
+FOUNDATION_EXPORT const unsigned char AVSTaggedAttributedString_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVSTaggedAttributedString_iOS/PublicHeader.h>
+
+
---
+++ AVSTaggedAttributedString/AVSTaggedAttributedString_macOS/AVSTaggedAttributedString_macOS.h
@@ -0,0 +1,19 @@
+//
+//  AVSTaggedAttributedString_macOS.h
+//  AVSTaggedAttributedString_macOS
+//
+//  Created by Andrei Sherstniuk on 8/8/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AVSTaggedAttributedString_macOS.
+FOUNDATION_EXPORT double AVSTaggedAttributedString_macOSVersionNumber;
+
+//! Project version string for AVSTaggedAttributedString_macOS.
+FOUNDATION_EXPORT const unsigned char AVSTaggedAttributedString_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVSTaggedAttributedString_macOS/PublicHeader.h>
+
+
