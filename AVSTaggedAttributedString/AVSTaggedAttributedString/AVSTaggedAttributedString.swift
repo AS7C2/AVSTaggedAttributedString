ca962784f9df4182aab090a13ceab49772373b14
Andrei Sherstniuk <sherstnyuk.a@gmail.com>
2016-08-08 16:20:25 +0300

    Update configuration.

---
+++ AVSTaggedAttributedString/AVSTaggedAttributedString/AVSTaggedAttributedString.swift
@@ -0,0 +1,13 @@
+//
+//  AVSTaggedAttributedString.swift
+//  AVSTaggedAttributedString
+//
+//  Created by Andrei Sherstniuk on 8/8/16.
+//
+//
+
+import Foundation
+
+func f1() -> Int {
+    return 100
+}
--- AVSTaggedAttributedString/AVSTaggedAttributedString_Tests/AVSTaggedAttributedStringTests.swift
+++ AVSTaggedAttributedString/AVSTaggedAttributedString_Tests/AVSTaggedAttributedStringTests.swift
@@ -1,36 +1,38 @@
 //
-//  AVSTaggedAttributedString_macOSTests.swift
-//  AVSTaggedAttributedString_macOSTests
+//  AVSTaggedAttributedStringTests.swift
+//  AVSTaggedAttributedString
 //
 //  Created by Andrei Sherstniuk on 8/8/16.
 //
 //
 
 import XCTest
-@testable import AVSTaggedAttributedString
+import AVSTaggedAttributedString
 
-class AVSTaggedAttributedString_macOSTests: XCTestCase {
-    
+class AVSTaggedAttributedStringTests: XCTestCase {
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(f1(), 100)
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
+
