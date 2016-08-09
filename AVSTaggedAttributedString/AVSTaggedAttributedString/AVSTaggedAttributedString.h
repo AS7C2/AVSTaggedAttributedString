b95c7af41028a84b1cb261faf61965cd45a53dcc
Andrei Sherstniuk <sherstnyuk.a@gmail.com>
2016-08-09 16:22:16 +0300

    Further update project structure.

--- AVSTaggedAttributedString/AVSTaggedAttributedString/AVSTaggedAttributedString.h
+++ AVSTaggedAttributedString/AVSTaggedAttributedString/AVSTaggedAttributedString.h
@@ -6,7 +6,7 @@
 //
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for AVSTaggedAttributedString_iOS.
 FOUNDATION_EXPORT double AVSTaggedAttributedString_iOSVersionNumber;
