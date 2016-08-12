18494b81503b6a4ee74c2a4561f3681d5e8f70e3
Andrei Sherstniuk <sherstnyuk.a@gmail.com>
2016-08-12 12:42:42 +0300

    Remove unnecessary "removeTag" parameter.

--- AVSTaggedAttributedString/AVSTaggedAttributedString/AVSTaggedAttributedString.swift
+++ AVSTaggedAttributedString/AVSTaggedAttributedString/AVSTaggedAttributedString.swift
@@ -9,24 +9,22 @@
 import Foundation
 
 extension NSMutableAttributedString {
-    func avs_addAttributes(attributes: [String: AnyObject], tag: String, removeTag: Bool) {
+    func avs_addAttributes(attributes: [String: AnyObject], tag: String) {
         let openTag = "<\(tag)>"
         let closeTag = "</\(tag)>"
         let string = self.mutableString
         let openRange = string.rangeOfString(openTag)
         let closeRange = string.rangeOfString(closeTag)
         if openRange.location != NSNotFound && closeRange.location != NSNotFound {
-            var innerLocation = openRange.location + openRange.length
-            let innerLength = closeRange.location - openRange.location - openRange.length
-            if removeTag {
+            if openRange.location + openRange.length <= closeRange.location {
+                var innerLocation = openRange.location + openRange.length
+                let innerLength = closeRange.location - openRange.location - openRange.length
                 innerLocation -= openRange.length;
                 self.replaceCharactersInRange(closeRange, withString: "")
                 self.replaceCharactersInRange(openRange, withString: "")
-            }
-            //if innerLength > 0 {
                 let innerRange = NSRange(location: innerLocation, length: innerLength)
                 self.addAttributes(attributes, range: innerRange)
-            //}
+            }
         }
     }
 }
--- AVSTaggedAttributedString/AVSTaggedAttributedStringTests/AVSTaggedAttributedStringTests.swift
+++ AVSTaggedAttributedString/AVSTaggedAttributedStringTests/AVSTaggedAttributedStringTests.swift
@@ -14,44 +14,59 @@ class AVSTaggedAttributedStringTests: XCTestCase {
 
 #if os(OSX)
     let redColor = NSColor.redColor()
+    let blueColor = NSColor.blueColor()
 #else
     let redColor = UIColor.redColor()
+    let blueColor = UIColor.blueColor()
 #endif
 
     func testEmpty() {
         let s = ""
         let mas = NSMutableAttributedString(string: s)
-        mas.avs_addAttributes([NSFontAttributeName: redColor], tag: "tag1", removeTag: false)
+        mas.avs_addAttributes([NSFontAttributeName: redColor], tag: "tag1")
         XCTAssertEqual(NSAttributedString(string: s), (mas.copy() as! NSAttributedString))
     }
 
     func testWithoutTags() {
         let s = "string"
         let mas = NSMutableAttributedString(string: s)
-        mas.avs_addAttributes([NSFontAttributeName: redColor], tag: "tag1", removeTag: true)
-        XCTAssertEqual(NSAttributedString(string: s), (mas.copy() as! NSAttributedString))
-    }
-
-    func testEmptyWithTagNoRemove() {
-        let s = "<tag1></tag1>"
-        let mas = NSMutableAttributedString(string: s)
-        mas.avs_addAttributes([NSFontAttributeName: redColor], tag: "tag1", removeTag: false)
+        mas.avs_addAttributes([NSForegroundColorAttributeName: redColor], tag: "tag1")
         XCTAssertEqual(NSAttributedString(string: s), (mas.copy() as! NSAttributedString))
     }
 
     func testEmptyWithTagRemove() {
         let s = "<tag1></tag1>"
         let mas = NSMutableAttributedString(string: s)
-        mas.avs_addAttributes([NSFontAttributeName: redColor], tag: "tag1", removeTag: true)
-        XCTAssertEqual(NSAttributedString(string: "", attributes: [NSFontAttributeName: redColor]), (mas.copy() as! NSAttributedString))
+        mas.avs_addAttributes([NSForegroundColorAttributeName: redColor], tag: "tag1")
+        XCTAssertEqual(NSAttributedString(string: "", attributes: [NSForegroundColorAttributeName: redColor]), (mas.copy() as! NSAttributedString))
     }
 
-    func testNoEmptyWithTagNoRemove() {
-        let s = "<tag1>string</tag1>"
+    func testNoEmptyWithSwitchedOpenAndCloseTagRemove() {
+        let s = "</tag1>string<tag1>"
         let mas = NSMutableAttributedString(string: s)
-        mas.avs_addAttributes([NSFontAttributeName: redColor], tag: "tag1", removeTag: false)
-        let expected = NSMutableAttributedString(string: s)
-        expected.addAttributes([NSFontAttributeName: redColor], range: NSRange(location: 6, length: 6))
+        mas.avs_addAttributes([NSForegroundColorAttributeName: redColor], tag: "tag1")
+        let expected = NSAttributedString(string: s)
+        XCTAssertEqual(expected, (mas.copy() as! NSAttributedString))
+    }
+
+    func testNoEmptyWithTwoTagsRemove() {
+        let s = "<tag1>string</tag1><tag2>text</tag2>"
+        let mas = NSMutableAttributedString(string: s)
+        mas.avs_addAttributes([NSForegroundColorAttributeName: redColor], tag: "tag1")
+        mas.avs_addAttributes([NSForegroundColorAttributeName: blueColor], tag: "tag2")
+        let expected = NSMutableAttributedString(string: "stringtext")
+        expected.addAttributes([NSForegroundColorAttributeName: redColor], range: NSRange(location:0, length: 6))
+        expected.addAttributes([NSForegroundColorAttributeName: blueColor], range: NSRange(location:6, length: 4))
+        XCTAssertEqual((expected.copy() as! NSAttributedString), (mas.copy() as! NSAttributedString))
+    }
+
+    func testWithTwoConsequentSameTagsRemove() {
+        let s = "<tag1>string</tag1> <tag1>text</tag1>"
+        let mas = NSMutableAttributedString(string: s)
+        mas.avs_addAttributes([NSForegroundColorAttributeName: redColor], tag: "tag1")
+        let expected = NSMutableAttributedString(string: "string text")
+        expected.addAttributes([NSForegroundColorAttributeName: redColor], range: NSRange(location:0, length: 6))
+        expected.addAttributes([NSForegroundColorAttributeName: redColor], range: NSRange(location:7, length: 4))
         XCTAssertEqual((expected.copy() as! NSAttributedString), (mas.copy() as! NSAttributedString))
     }
 }
