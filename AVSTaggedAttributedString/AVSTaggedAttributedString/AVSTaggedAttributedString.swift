b3add5973eaee462031b625a5deedc07e60d195d
Andrei Sherstniuk <sherstnyuk.a@gmail.com>
2016-08-12 13:05:32 +0300

    Refactor

--- AVSTaggedAttributedString/AVSTaggedAttributedString/AVSTaggedAttributedString.swift
+++ AVSTaggedAttributedString/AVSTaggedAttributedString/AVSTaggedAttributedString.swift
@@ -8,14 +8,27 @@
 
 import Foundation
 
+extension NSRange {
+    var isFound: Bool {
+        get {
+            return self.location != NSNotFound
+        }
+    }
+
+    func isBefore(range range: NSRange) -> Bool {
+        return self.location + self.length <= range.location
+    }
+}
+
 extension NSMutableAttributedString {
     func avs_addAttributes(attributes: [String: AnyObject], tag: String) {
         let openTag = "<\(tag)>"
         let closeTag = "</\(tag)>"
+
         var string = self.mutableString
         var openRange = string.rangeOfString(openTag)
         var closeRange = string.rangeOfString(closeTag)
-        while openRange.location != NSNotFound && closeRange.location != NSNotFound && openRange.location + openRange.length <= closeRange.location {
+        while openRange.isFound && closeRange.isFound && openRange.isBefore(range: closeRange) {
             var innerLocation = openRange.location + openRange.length
             let innerLength = closeRange.location - openRange.location - openRange.length
             innerLocation -= openRange.length;
